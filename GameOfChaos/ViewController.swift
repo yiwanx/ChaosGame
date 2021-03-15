0fb9d6dba73e0e27397d600135d90f4225c06c5c
Ivan Petukhov <yiwanv@gmail.com>
2021-03-15 10:45:10 +0300

    Initial Commit

---
+++ GameOfChaos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GameOfChaos
+//
+//  Created by 1 on 15.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
